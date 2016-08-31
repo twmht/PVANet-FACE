0b7439a11671ef41ebfa3603eb1f73514f70dcef
Sanghoon Hong <sanghoon.hong@intel.com>
2016-08-31 15:03:48 +0900

    Make anchor ratio to be configurable in a prototxt

--- lib/rpn/anchor_target_layer.py
+++ lib/rpn/anchor_target_layer.py
@@ -26,7 +26,8 @@ class AnchorTargetLayer(caffe.Layer):
     def setup(self, bottom, top):
         layer_params = yaml.load(self.param_str_)
         anchor_scales = layer_params.get('scales', (8, 16, 32))
-        self._anchors = generate_anchors(scales=np.array(anchor_scales))
+        anchor_ratios = layer_params.get('ratios', ((0.5, 1, 2)))
+        self._anchors = generate_anchors(ratios=anchor_ratios, scales=np.array(anchor_scales))
         self._num_anchors = self._anchors.shape[0]
         self._feat_stride = layer_params['feat_stride']
 
--- lib/rpn/proposal_layer.py
+++ lib/rpn/proposal_layer.py
@@ -27,7 +27,8 @@ class ProposalLayer(caffe.Layer):
 
         self._feat_stride = layer_params['feat_stride']
         anchor_scales = layer_params.get('scales', (8, 16, 32))
-        self._anchors = generate_anchors(scales=np.array(anchor_scales))
+        anchor_ratios = layer_params.get('ratios', ((0.5, 1, 2)))
+        self._anchors = generate_anchors(ratios=anchor_ratios, scales=np.array(anchor_scales))
         self._num_anchors = self._anchors.shape[0]
 
         if DEBUG:

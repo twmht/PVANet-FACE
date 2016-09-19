4cbd40ca1daae92f13ee7c61c4712b2e99433220
Kye-Hyeon Kim <kyehyeon.mlg@gmail.com>
2016-09-19 19:07:44 +0900

    Update the download script for ImageNet pretrained models

--- models/pvanet/download_imagenet_models.sh
+++ models/pvanet/download_imagenet_models.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-wget  -O models/pvanet/imagenet/test.model
-wget  -O models/pvanet/imagenet/original.model
+wget https://www.dropbox.com/s/2xsfg8vqmhv9p84/test.model?dl=0 -O models/pvanet/imagenet/test.model
+wget https://www.dropbox.com/s/l11v2uh05pjejws/original.model?dl=0 -O models/pvanet/imagenet/original.model
